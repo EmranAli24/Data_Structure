6096079ef8b3f5c1aeed28c9c2bb5e81efd41ed0
EmranAli24 <emran.ali.csecu@gmail.com>
2025-05-25 10:37:37 +0600

    Heap related programs added.

---
+++ Heap/MaxHeap_PracticeQ1.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class BinaryHeap {
+public:
+    int capacity; 
+    int size; 
+    int *arr; 
+
+    BinaryHeap(int cap) {
+        capacity = cap;
+        size = 0;
+        arr = new int[capacity];
+    }
+
+    int parent(int i) { return (i - 1) / 2; }
+    int left(int i) { return 2 * i + 1; }
+    int right(int i) { return 2 * i + 2; }
+
+    // Insert: Now ensures parent >= child (Max Heap property)
+    void Insert(int x) {
+        if (size == capacity) {
+            cout << "Binary Heap Overflow" << endl;
+            return;
+        }
+        arr[size] = x;
+        int k = size;
+        size++;
+
+        // Changed to arr[parent(k)] < arr[k] (for Max Heap)
+        while (k != 0 && arr[parent(k)] < arr[k]) {
+            swap(&arr[parent(k)], &arr[k]);
+            k = parent(k);
+        }
+    }
+
+    // Heapify: Now ensures parent >= children (Max Heap)
+    void Heapify(int ind) {
+        int ri = right(ind);
+        int li = left(ind);
+        int largest = ind;  // Renamed 'smallest' to 'largest' for clarity
+
+        if (li < size && arr[li] > arr[largest])  // Changed to '>'
+            largest = li;
+        if (ri < size && arr[ri] > arr[largest])  // Changed to '>'
+            largest = ri;
+
+        if (largest != ind) {
+            swap(&arr[ind], &arr[largest]);
+            Heapify(largest);
+        }
+    }
+
+    // Renamed getMin() to getMax()
+    int getMax() { return arr[0]; }
+
+    // Renamed ExtractMin() to ExtractMax()
+    int ExtractMax() {
+        if (size <= 0) return INT_MIN;  // Changed from INT_MAX to INT_MIN
+        if (size == 1) {
+            size--;
+            return arr[0];
+        }
+        int maxi = arr[0];
+        arr[0] = arr[size - 1];
+        size--;
+        Heapify(0);
+        return maxi;
+    }
+
+    // Decreasekey renamed to IncreaseKey (since we're in Max Heap)
+    void IncreaseKey(int i, int val) {
+        arr[i] = val;
+        // Changed to arr[parent(i)] < arr[i] (Max Heap fix)
+        while (i != 0 && arr[parent(i)] < arr[i]) {
+            swap(&arr[parent(i)], &arr[i]);
+            i = parent(i);
+        }
+    }
+
+    void Delete(int i) {
+        IncreaseKey(i, INT_MAX);  // Use INT_MAX to ensure it bubbles up
+        ExtractMax();
+    }
+
+    void swap(int *x, int *y) {
+        int temp = *x;
+        *x = *y;
+        *y = temp;
+    }
+
+    void print() {
+        for (int i = 0; i < size; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+};
+
+int main() {
+    BinaryHeap h(20);
+    h.Insert(44);
+    h.Insert(35);
+    h.Insert(42);
+    h.Insert(26);
+    h.Insert(33);
+    h.Insert(19);
+    h.Insert(27);
+    h.Insert(10);
+    h.Insert(14);
+    h.Insert(31);
+    // cout << "Max value is " << h.getMax() << endl;  // Now outputs max
+    // h.Insert(9);
+    // cout << "Max value is " << h.getMax() << endl;
+    // h.IncreaseKey(3, 10);  // Renamed from Decreasekey
+    // cout << "Max value is " << h.getMax() << endl;
+    // h.ExtractMax();
+    // cout << "Max value is " << h.getMax() << endl;
+    // h.Delete(0);
+    // cout << "Max value is " << h.getMax() << endl;
+    h.print();
+    
+
+    return 0;
+}
